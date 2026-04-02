444aaa0501080d53079e13fecbf36e091ad98c4a
hxasset <hxasset0307@gmail.com>
2026-04-02 16:01:38 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,100 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-l",
+      "name": "L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5497842313753907,
+        "pitch": 0.058485387849099624,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.737949038672042,
+          "pitch": -0.06346986515264241,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4808560259860695,
+          "pitch": -0.3686572664933827,
+          "title": "匯幸資產版權所有",
+          "text": "<span class=\"NormalTextRun SCXW19587319 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; background-color: rgb(255, 255, 255);\">&lt;</span><span class=\"NormalTextRun SCXW19587319 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; background-color: rgb(255, 255, 255);\">202604</span><span class=\"NormalTextRun SCXW19587319 BCX0\" style=\"-webkit-user-drag: none; -webkit-tap-highlight-color: transparent; margin: 0px; padding: 0px; user-select: text; color: rgb(0, 0, 0); font-family: &quot;Microsoft JhengHei&quot;, &quot;Microsoft JhengHei_EmbeddedFont&quot;, &quot;Microsoft JhengHei_MSFontService&quot;, sans-serif; font-size: 14.6667px; font-variant-ligatures: none; background-color: rgb(255, 255, 255);\">&gt;</span>"
+        },
+        {
+          "yaw": -1.485896479877301,
+          "pitch": -0.25407634746640184,
+          "title": "格局圖",
+          "text": "<img src='floorplan.jpg"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.4007133486721557,
+        "pitch": 0.053306365087976815,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8506052735743097,
+          "pitch": 0.016882911056365302,
+          "rotation": 0,
+          "target": "0-l"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
